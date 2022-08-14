5992d3017b663134b82a41d44241dc72be9a3447
German Suslin <germansuslin@gmail.com>
2022-08-14 19:23:13 +1000

    test trash

--- Generator.py
+++ Generator.py
@@ -1,7 +1,6 @@
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler, MinMaxScaler  # Нормировщики
 
-
 import random
 
 import tensorflow
@@ -14,7 +13,6 @@ from tensorflow.keras.layers import concatenate, \
     Flatten, Conv1D, Conv2D, \
     LSTM  # Стандартные слои
 
-
 import matplotlib.pyplot as plt
 
 import pandas as pd
@@ -26,7 +24,6 @@ range_gk_loss = 1.5
 range_pe_loss = 1
 range_ds_loss = 50
 range_dtp_loss = 20
-
 
 errors = [range_ggkp_loss, range_gk_loss, range_pe_loss,
           range_ds_loss, range_dtp_loss]
@@ -54,6 +51,7 @@ class Generator(tensorflow.keras.utils.Sequence):
     '''
     Генератор батчей с x - x_len*batch_size и y - batch_size
     '''
+
     def __init__(self, x_data,
                  y_data_coll,
                  y_data_rest,
@@ -80,7 +78,7 @@ class Generator(tensorflow.keras.utils.Sequence):
         :param y_columns: list of inst [0-3]
         пока не используется - но выбор колонок
         '''
-        self.x_data = x_data[:,x_columns]
+        self.x_data = x_data[:, x_columns]
         self.y_data_coll = y_data_coll
         self.y_data_rest = y_data_rest
         self.x_columns = x_columns
@@ -90,6 +88,7 @@ class Generator(tensorflow.keras.utils.Sequence):
         self.norm_fit = []
         self.norm_y_fit = []
         self.only_colls = only_colls
+
     def info(self):
         '''
         Вывод информации о датафрейма
@@ -103,8 +102,9 @@ class Generator(tensorflow.keras.utils.Sequence):
             for column in range(len(self.x_data[0])):
                 self.x_data[row, column] = round(self.x_data[row, column] + \
                                                  random.uniform(-errors_value[column],
-                                                                errors_value[column]),3)
+                                                                errors_value[column]), 3)
         print(self.x_data.shape)
+
     def normalize(self):
         # нормализация каждого столбца данных
         for i in range(len(self.x_columns)):
@@ -119,34 +119,39 @@ class Generator(tensorflow.keras.utils.Sequence):
         # возвращает список нормализаторов, чтобы потом нормализировать
         # тестовые данные
         return self.norm_fit, self.norm_y_fit
+
     def normalize_test(self, norm_fit, norm_y_fit):
         for i in range(len(self.x_columns)):
             self.x_data = norm_fit[i].transform(self.x_data)
         for i in range(2):
             self.y_data_rest = norm_y_fit[i].transform(self.y_data_rest)
+
     def __get_data(self, x_batch, y_batch_coll, y_batch_rest):
         # Разбиваем наш батч на сеты
         # Определим максимальный индекс
         form = x_batch.shape[0] - self.length
-        x = [x_batch[i:i+self.length] for i in range(form)]
+        x = [x_batch[i:i + self.length] for i in range(form)]
         y_coll = [y_batch_coll[i] for i in range(form)]
         y_rest = [y_batch_rest[i] for i in range(form)]
-        return  np.array(x), np.array(y_coll), np.array(y_rest)
+        return np.array(x), np.array(y_coll), np.array(y_rest)
+
     def __len__(self):
         return self.x_data.shape[0] // (self.batch_size + self.length)
 
     def __getitem__(self, index):
         # Формирование выборки батчей
         # Берём значения от 0 до размера батча + длина выборки предсказания
-        x_batch = self.x_data[index*self.batch_size:
-                                (index+1)*self.batch_size + self.length]
+        x_batch = self.x_data[index * self.batch_size:
+                              (index + 1) * self.batch_size + self.length]
 
-        y_batch_coll = self.y_data_coll[index*self.batch_size + self.length - 1:
-                                (index+1)*self.batch_size+self.length - 1]
-        y_batch_rest = self.y_data_rest[index*self.batch_size + self.length - 1:
-                                (index+1)*self.batch_size+self.length - 1]
-        print(y_batch_rest.shape, x_batch.shape)
+        y_batch_coll = self.y_data_coll[index * self.batch_size + self.length:
+                                        (index + 1) * self.batch_size + self.length]
+
+        y_batch_rest = self.y_data_rest[index * self.batch_size + self.length:
+                                        (index + 1) * self.batch_size + self.length]
+        print(y_batch_rest.shape, y_batch_coll.shape, x_batch.shape)
         x, y_coll, y_rest = self.__get_data(x_batch, y_batch_coll, y_batch_rest)
+
         if self.only_colls == True:
             return x, y_coll
         else:
