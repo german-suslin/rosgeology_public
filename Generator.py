135ab025da54ee751e9fc88e59ffeebb87ddb36e
German Suslin <germansuslin@gmail.com>
2022-08-27 21:14:57 +1000

    Исправлена ошибка нормировки.

--- Generator.py
+++ Generator.py
@@ -108,13 +108,16 @@ class Generator(tensorflow.keras.utils.Sequence):
     def normalize(self):
         # нормализация каждого столбца данных
         for i in range(len(self.x_columns)):
+            x = self.x_data[:, i].reshape(-1, 1)
             xScaler = StandardScaler()
-            xScaler.fit(self.x_data)
+            xScaler.fit(x)
             self.norm_fit.append(xScaler)
-            self.x_data = xScaler.transform(self.x_data)
-        for i in range(2):
+            self.x_data[:, i] = np.array(xScaler.transform(x)).reshape(-1)
+        for i in range(len(self.y_data_rest[0])):
+            y = self.y_data_rest[:, i].reshape(-1, 1)
             yScaler = StandardScaler()
-            yScaler.fit(self.y_data_rest)
+            yScaler.fit(y)
+            self.y_data_rest[:, i] = np.array(xScaler.transform(y)).reshape(-1)
             self.norm_y_fit.append(yScaler)
         # возвращает список нормализаторов, чтобы потом нормализировать
         # тестовые данные
@@ -122,9 +125,11 @@ class Generator(tensorflow.keras.utils.Sequence):
 
     def normalize_test(self, norm_fit, norm_y_fit):
         for i in range(len(self.x_columns)):
-            self.x_data = norm_fit[i].transform(self.x_data)
+            x = self.x_data[:, i].reshape(-1, 1)
+            self.x_data[:, i] = np.array(norm_fit[i].transform(x)).reshape(-1)
         for i in range(len(norm_y_fit)):
-            self.y_data_rest = norm_y_fit[i].transform(self.y_data_rest)
+            y = self.y_data_rest[:, i].reshape(-1, 1)
+            self.y_data_rest[:, i] = np.array(norm_y_fit[i].transform(y)).reshape(-1)
 
     def __get_data(self, x_batch, y_batch_coll, y_batch_rest):
         # Разбиваем наш батч на сеты
